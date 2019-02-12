b2d822713b2d89acbfa7ba01e321b4f89b05f6f3
Vasidovi <vasido@gmail.com>
2019-02-12 17:08:45 +0200

    rivers eraser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var mousePressed = false;
 var lastX, lastY;
 var ctx;
 var img = new Image();
+let riversCoordinates = [];
 
 // img.onload = function(){
 //     image.src = this.src;   
@@ -12,7 +13,11 @@ img.src = "./images/mountains-trial.png";
 ctx.canvas.width = window.innerWidth * 0.95;
 ctx.canvas.height = window.innerHeight * 0.95;
 
-
+if (!Array.prototype.last) {
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    };
+};
 
 
 $('#myCanvas').mousedown(function (e) {
@@ -51,8 +56,15 @@ $('#corrector-btn').click(function () {
     activeTool = "corrector";
 });
 
+$('#eraser-btn').click(function () {
+    activeTool = "eraser";
+});
+
+
 
 function Draw(x, y, isDown) {
+
+    ctx.save();
 
     if (activeTool == "mountains") {
         drawMountains(x, y, isDown);
@@ -60,9 +72,11 @@ function Draw(x, y, isDown) {
         drawRivers(x, y, isDown);
     } else if (activeTool == "corrector") {
         correct(x, y, isDown);
+    } else if ( activeTool =="eraser") {
+        undoRiver(x,y);
     }
+    ctx.restore();
 };
-
 
 function drawMountains(x, y, isDown) {
     const size = 35;
@@ -86,19 +100,69 @@ function correct(x, y, isDown) {
     const size = 20;
 
     if (isDown) {
-
-            ctx.beginPath();
-            ctx.strokeStyle = "#fff";
-            ctx.lineWidth = size;
-            ctx.lineJoin = "round";
-            ctx.moveTo(lastX, lastY);
-            ctx.lineTo(x, y);
-
-            ctx.closePath();
-            ctx.stroke();
-        }
+        ctx.beginPath();
+        ctx.strokeStyle = "#fff";
+        ctx.lineWidth = size;
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
     lastX = x;
     lastY = y;
+}
+
+function undoRiver(x, y) {
+
+    let river = findRiver(x, y, riversCoordinates);
+    riversCoordinates.splice(riversCoordinates.indexOf(river), 1);
+    clearArea();
+    redrawRivers(riversCoordinates);
+}
+
+function redrawRivers(riversCoordinates){
+    if (riversCoordinates.length > 0) {
+        // iterate through all the rivers
+        for (let i = 0; i < riversCoordinates.length; i++) {
+            let river = riversCoordinates[i];
+            for (let j = 1; j < river.length-1; j++) {           
+            const startX = river[j-1].x;
+            const startY = river[j-1].y;
+            const endX = river[j].x;
+            const endY = river[j].y;
+            ctx.beginPath();
+            ctx.strokeStyle = "#000";
+            // ctx.lineWidth = size;
+            ctx.lineJoin = "round";
+            ctx.moveTo(startX, startY);
+            ctx.lineTo(endX, endY);
+            ctx.stroke();
+            }
+        }
+    }         
+}
+
+function findRiver(x, y, riversCoordinates) {
+
+    if (riversCoordinates.length > 0) {
+        let foundRiver = riversCoordinates[0];
+        let minDifference = Math.abs(x - foundRiver[0].x) + Math.abs(y - foundRiver[0].y);
+        // iterate through all the rivers
+        for (let i = 0; i < riversCoordinates.length; i++) {
+            // iterate through all the river coords
+            let river = riversCoordinates[i];
+            for (let j = 0; j < river.length; j++) {
+                const difference = Math.abs(x - river[j].x) + Math.abs(y - river[j].y);
+                if (difference < minDifference) {
+                    minDifference = difference;
+                    foundRiver = river;
+                }
+            }
+        }
+        return foundRiver;
+    } else {
+        return null;
+    }
 }
 
 
@@ -110,20 +174,20 @@ function drawRivers(x, y, isDown) {
 
         if (Math.abs(x - prevX) >= size || Math.abs(y - prevY) >= size) {
             ctx.beginPath();
-            // // ctx.strokeStyle = ;
-            // // ctx.lineWidth = $('#selWidth').val();
             ctx.lineJoin = "round";
             ctx.moveTo(prevX, prevY);
+            riversCoordinates.last().push({
+                "x": prevX,
+                "y": prevY
+            });
             prevX = x + (Math.random() - 0.5) * Math.abs(prevX - x) * distortion;
-            console.log(Math.abs(prevX - x) * distortion);
             prevY = y + (Math.random() - 0.5) * Math.abs(prevY - y) * distortion;
             ctx.lineTo(prevX, prevY);
-
-            ctx.closePath();
             ctx.stroke();
         }
 
     } else {
+        riversCoordinates.push([]);
         prevX = x;
         prevY = y;
     }
